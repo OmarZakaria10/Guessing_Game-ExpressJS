dd078df3a6bc623995c6661319fc5656540049a5
OmarZakaria10 <ok2m32@gmail.com>
2024-08-23 21:29:09 +0300

    new update

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const mongo = require('./util/mongoose')
+const mongo = require("./util/mongoose");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const gameRoutes = require('./routes/game');
+const gameRoutes = require("./routes/game");
 
 const app = express();
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(gameRoutes);
-
 
 app.listen(3000);
